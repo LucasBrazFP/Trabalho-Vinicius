d550879adb68fe0d7d9cf122a22b5b7ca0a8408b
Braz <51178207+LucasBrazFP@users.noreply.github.com>
2025-11-14 17:30:21 -0300

    Add files via upload

---
+++ import_random_para.py
@@ -0,0 +1,85 @@
+import random
+import shutil
+import os
+from pathlib import Path
+from multiprocessing import Pool, cpu_count
+import time
+import heapq
+import math
+
+def gerar_numeros(tamanho):
+
+    tamanho_numero = 10  
+
+    num_numeros = tamanho // tamanho_numero
+
+    with open("numeros_grandes.txt", "w") as arquivo:
+        for _ in range(num_numeros):
+
+            numero = random.randint(0, 1000000000)
+            arquivo.write(f"{numero}\n")
+
+    tamanho_gerado = tamanho / (1024**2)
+    print(f"Arquivo gerado com aproximadamente {tamanho_gerado:.2f} MB")
+    return tamanho_gerado
+
+def ordenar_numeros():
+    print("Lendo números do arquivo...")
+    with open("numeros_grandes.txt", "r") as arquivo:
+        numeros = [int(linha.strip()) for linha in arquivo]
+    
+    if not numeros:
+        print("Arquivo vazio. Nada para ordenar.")
+        return
+
+    print(f"Ordenando números com paralelismo ({cpu_count()} núcleos)...")
+    inicio = time.time()
+
+    
+    num_processos = min(cpu_count(), len(numeros))
+    if num_processos <= 0:
+        num_processos = 1
+
+    
+    tamanho_chunk = math.ceil(len(numeros) / num_processos)
+    chunks = [numeros[i:i + tamanho_chunk] for i in range(0, len(numeros), tamanho_chunk)]
+
+    
+    with Pool(processes=num_processos) as pool:
+        chunks_ordenados = pool.map(sorted, chunks)
+
+    numeros_ordenados_iter = heapq.merge(*chunks_ordenados)
+
+    tempo_decorrido = time.time() - inicio
+
+    print("Salvando números ordenados em arquivo separado...")
+    with open("numeros_ordenados.txt", "w") as arquivo:
+        for numero in numeros_ordenados_iter:
+            arquivo.write(f"{numero}\n")
+
+    print(f"Números ordenados salvos em 'numeros_ordenados.txt' com sucesso!")
+    print(f"Tempo de ordenação: {tempo_decorrido:.2f} segundos")
+
+def mover_para_area_trabalho():
+    print("Movendo arquivos para Área de Trabalho...")
+    
+    # Enviar para area de trab
+    desktop = str(Path.home() / "Desktop")
+    
+    # Move numeros_grandes.txt
+    if os.path.exists("numeros_grandes.txt"):
+        shutil.move("numeros_grandes.txt", os.path.join(desktop, "numeros_grandes.txt"))
+        print("numeros_grandes.txt movido para Desktop")
+    
+    # Move numeros_ordenados.txt
+    if os.path.exists("numeros_ordenados.txt"):
+        shutil.move("numeros_ordenados.txt", os.path.join(desktop, "numeros_ordenados.txt"))
+        print("numeros_ordenados.txt movido para Desktop")
+    
+    print(f"Arquivos movidos com sucesso para: {desktop}")
+
+if __name__ == '__main__':
+    #gerando a lista
+    gerar_numeros(1*1024**3)
+    ordenar_numeros()
+    mover_para_area_trabalho()
